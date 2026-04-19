58cc731e4a9389c6af511c8d10dc181860cebf33
Jai-priyadarshi <jai98834@gmail.com>
2026-04-19 15:26:53 +0530

    Add start.sh for Render deploy

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-# Start Django backend
-source venv/bin/activate
-cd backend && python manage.py runserver &
-DJANGO_PID=$!
-
-# Start React frontend
-cd ../frontend && npm run dev &
-VITE_PID=$!
-
-echo "Backend running at http://localhost:8000"
-echo "Frontend running at http://localhost:5173"
-echo ""
-echo "Press Ctrl+C to stop both servers"
-
-trap "kill $DJANGO_PID $VITE_PID" EXIT
-wait
+#!/usr/bin/env bash
+set -e
+cd backend
+gunicorn backend.wsgi:application --workers 2 --bind 0.0.0.0:$PORT --timeout 120
